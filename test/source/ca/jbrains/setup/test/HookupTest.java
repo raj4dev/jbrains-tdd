36cf03471f62103bc3e7f48badbb49f4ae821b1c
rb <rb@email.com>
2016-05-03 23:36:37 -0700

    IDEA test does what we want, and we have one button testing.

---
+++ test/source/ca/jbrains/setup/test/HookupTest.java
@@ -0,0 +1,11 @@
+package ca.jbrains.setup.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HookupTest {
+    @Test
+    public void isThisThingOn() throws Exception {
+
+    }
+}
