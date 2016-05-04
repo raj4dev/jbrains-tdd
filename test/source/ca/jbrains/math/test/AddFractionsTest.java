8a9529732a08e8e7937217978b0be39b2bc168f3
rb <rb@email.com>
2016-05-04 12:31:27 -0700

    We now add 0 to 0.

---
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -0,0 +1,31 @@
+package ca.jbrains.math.test;
+
+import ca.jbrains.math.Fraction;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class AddFractionsTest {
+    @Test
+    public void zeroPlusZero() throws Exception {
+        Fraction sum = new Fraction(0).plus(new Fraction(0));
+        assertEquals(0, sum.intValue()); /*Sum of two fractions*/
+    }
+}
+
+
+/*
+* -------------------------
+* NOTES
+* -------------------------
+*
+* zeroPlusZero() -
+* How to check the equality of fractions using assertEquals ?
+ * 1. Create an equals method for Fraction ???
+  * NO ! Focus on one thing at a time, i.e. adding two fractions. Let equals be your next TODO.
+  * Besides, we havent even created the fraction class yet & don't know what it should be like.
+  * Also, we don't want to make a mistake in equals to cause mistakes in add.
+  *
+ * 2. Ask the fraction sum its int value (somehow) instead of converting it to a number.
+*
+* */
