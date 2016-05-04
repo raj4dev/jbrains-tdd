136913b424bc34485127056c17be12425b007b1c
rb <rb@email.com>
2016-05-04 13:26:14 -0700

    We now add negative integers, even when it gives us a negative result.

--- test/source/ca/jbrains/math/test/AddFractionsTest.java
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -30,6 +30,12 @@ public class AddFractionsTest {
         final Fraction sum = new Fraction(3).plus(new Fraction(4));
         assertEquals(7, sum.intValue());
     }
+
+    @Test
+    public void negativeInputsAndNegativeOutput() throws Exception {
+        final Fraction sum = new Fraction(-3).plus(new Fraction(1));
+        assertEquals(-2, sum.intValue());
+    }
 }
 
 
