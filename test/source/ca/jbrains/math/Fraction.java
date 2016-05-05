59af3030d4ba3bcf69640d7293ffca22f1cc734b
rb <rb@email.com>
2016-05-05 11:24:18 -0700

    we no longer fake the denominator.

--- test/source/ca/jbrains/math/Fraction.java
+++ test/source/ca/jbrains/math/Fraction.java
@@ -1,18 +1,22 @@
 package ca.jbrains.math;
 
 public class Fraction {
-    private int integerValue;
+    private final int denominator;
+    private final int integerValue;
 
     public Fraction(int integerValue) {
         this.integerValue = integerValue;
+        this.denominator = 1;
     }
 
     public Fraction(int numerator, int denominator) {
-
+        this.integerValue = numerator;
+        this.denominator = denominator;
     }
 
     public Fraction plus(Fraction that){
-        return new Fraction(this.integerValue + that.integerValue);
+        return new Fraction(this.integerValue + that.integerValue, denominator); /*default denominator of 1 ensures that
+         old tests will pass.*/
     }
 
     public int intValue() {
@@ -24,6 +28,6 @@ public class Fraction {
     }
 
     public int getDenominator() {
-        return 5;
+        return denominator;
     }
 }
