defb8f1ac4ff724a4b284e34c43ee6d429ee2f8f
rb <rb@email.com>
2016-05-10 15:19:03 -0700

    we can add fractions with different denominators, as long as we don't have to reduce the result.

--- test/source/ca/jbrains/math/Fraction.java
+++ test/source/ca/jbrains/math/Fraction.java
@@ -5,7 +5,7 @@ public class Fraction {
     private final int denominator;
 
     public Fraction(int integerValue) {
-        this(integerValue,1);
+        this(integerValue, 1);
     }
 
     public Fraction(int numerator, int denominator) {
@@ -13,16 +13,22 @@ public class Fraction {
         this.denominator = denominator;
     }
 
-    public Fraction plus(Fraction that){
-        return new Fraction(this.numerator + that.numerator, denominator); /*default denominator of 1 ensures that
+    public Fraction plus(Fraction that) {
+        if (this.denominator != that.denominator) {
+            int sumNumerator = (this.numerator * that.denominator) + (that.numerator * this.denominator);
+            int sumDenominator = (this.denominator * that.denominator);
+            return (new Fraction(sumNumerator, sumDenominator));
+        } else {
+            return new Fraction(this.numerator + that.numerator, denominator); /*default denominator of 1 ensures that
          old tests will pass.*/
+        }
     }
 
     @Override
     public boolean equals(Object other) {
-        if(other instanceof Fraction){
+        if (other instanceof Fraction) {
             Fraction that = (Fraction) other;
-            return  this.numerator == that.numerator && this.denominator == that.denominator;
+            return this.numerator == that.numerator && this.denominator == that.denominator;
         }
         return false;
     }
@@ -33,7 +39,7 @@ public class Fraction {
     }
 
     @Override
-    public String toString(){
-        return String.format("%d/%d",numerator, denominator);
+    public String toString() {
+        return String.format("%d/%d", numerator, denominator);
     }
 }
