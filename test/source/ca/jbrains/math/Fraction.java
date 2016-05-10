601f2945c6456ab83b49192e65933b73bca00c06
rb <rb@email.com>
2016-05-10 14:19:08 -0700

    Implemented hashCode() consistently with equals.

--- test/source/ca/jbrains/math/Fraction.java
+++ test/source/ca/jbrains/math/Fraction.java
@@ -28,6 +28,11 @@ public class Fraction {
     }
 
     @Override
+    public int hashCode() {
+        return numerator * 19 + denominator;
+    }
+
+    @Override
     public String toString(){
         return String.format("%d/%d",numerator, denominator);
     }
