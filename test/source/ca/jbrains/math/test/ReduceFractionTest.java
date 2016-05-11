17c9f0e4134a71b96a36db0d00625429ca1c31f0
rb <rb@email.com>
2016-05-11 15:03:33 -0700

    we already reduced the fraction 0/anything correctly.

--- test/source/ca/jbrains/math/test/ReduceFractionTest.java
+++ test/source/ca/jbrains/math/test/ReduceFractionTest.java
@@ -16,4 +16,14 @@ public class ReduceFractionTest {
     public void reduceToNotWholeNumber() throws Exception {
         assertEquals(new Fraction(3, 4), new Fraction(6, 8));
     }
+
+    @Test
+    public void reduceToWholeNumber() throws Exception {
+        assertEquals(new Fraction(6), new Fraction(24, 4));
+    }
+
+    @Test
+    public void reduceZero() throws Exception {
+        assertEquals(new Fraction(0), new Fraction(0, 172635));
+    }
 }
