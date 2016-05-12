d59e3963e27d5dabceef971f5ca4816a6b48aa95
rb <rb@email.com>
2016-05-11 17:52:57 -0700

    wrote a handful more examples for reducing while adding fractions. Found a missing case for GCD.

--- test/source/ca/jbrains/math/test/AddFractionsTest.java
+++ test/source/ca/jbrains/math/test/AddFractionsTest.java
@@ -1,6 +1,7 @@
 package ca.jbrains.math.test;
 
 import ca.jbrains.math.Fraction;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static junit.framework.TestCase.assertEquals;
@@ -42,6 +43,39 @@ public class AddFractionsTest {
     @Test
     public void differentDenominatorsWithoutReducing() throws Exception {
         assertEquals(new Fraction(5,6), new Fraction(1,2).plus(new Fraction(1,3)));
+    }
+
+    @Test
+    public void reduceResultToWholeNumber() throws Exception {
+        assertEquals(new Fraction(1), new Fraction(1,3).plus(new Fraction(2,3)));
+    }
+
+    @Test
+    public void oneDenominatorIsAMultipleOfTheOther() throws Exception {
+        assertEquals(new Fraction(11,8), new Fraction(3,4).plus(new Fraction(5,8)));
+    }
+
+    @Test
+    public void commonFactorInDenominators() throws Exception {
+        assertEquals(new Fraction(11,18), new Fraction(1,6).plus(new Fraction(4,9)));
+    }
+
+    @Test
+    public void reduceResultEvenWhenDenominatorsAreTheSame() throws Exception {
+        assertEquals(new Fraction(3,2), new Fraction(3,4).plus(new Fraction(3,4)));
+    }
+
+    @Test
+    public void negativeFractionAndReducing() throws Exception {
+        assertEquals(new Fraction(1,2), new Fraction(-1,4).plus(new Fraction(3,4)));
+        //BAD Practice ! Having more than 1 test in a test case.
+        assertEquals(new Fraction(-1,8), new Fraction(3,8).plus(new Fraction(-1,2)));
+    }
+
+    @Test
+    @Ignore("Constructor does not yet avoid negative denominators.")
+    public void negativeSignsEverywhere() throws Exception {
+        assertEquals(new Fraction(1,2), new Fraction(1,-4).plus(new Fraction(-3,-4)));
     }
 }
 
